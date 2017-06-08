e366442f13e955c452bb2bad04065195bf437426
BRYCEN-VN\q_minh <lequangminh28@gmail.com>
2017-06-08 10:33:16 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (C) 2014-2015, phamtuan Research Inc.
+//  
+// All rights are reserved. Reproduction or transmission in whole or in part, in any form or by
+// any means, electronic, mechanical or otherwise, is prohibited without the prior written
+// consent of the copyright owner.
+// ---------------------------------------------------------------------------------
+
+#region
+
+using System;
+using System.Windows.Forms;
+using xDialog;
+
+#endregion
+
+namespace MessageBoxDemo
+{
+    public partial class frmDemo : Form
+    {
+        public frmDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            MsgBox.Show("Are you sure you want to exit?", "Eror", MsgBox.Buttons.OKCancel, MsgBox.Icon.Error,
+                MsgBox.AnimateStyle.ZoomIn);
+            DialogResult result = MsgBox.Show("Are you sure you want to exit?", "Exit", MsgBox.Buttons.YesNoCancel,
+                MsgBox.Icon.Shield, MsgBox.AnimateStyle.ZoomIn);
+
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("Exiting now");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (C) 2014-2015, phamtuan Research Inc.
+//  
+// All rights are reserved. Reproduction or transmission in whole or in part, in any form or by
+// any means, electronic, mechanical or otherwise, is prohibited without the prior written
+// consent of the copyright owner.
+// ---------------------------------------------------------------------------------
+
+#region
+
+using System;
+using System.Windows.Forms;
+
+#endregion
+
+namespace MessageBoxDemo
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmDemo());
+        }
+    }
+}
